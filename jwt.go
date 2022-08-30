0d76dc1fc3e3bcf7fe238ebcf5d6d0206079bdea
Marcelo Rydel <marcelo.rydel@compass.com>
2022-08-30 16:19:49 -0300

    Vers v3.0.26

---
+++ jwt.go
@@ -0,0 +1,47 @@
+package glide
+
+import (
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type claims struct {
+	Scopes []string `json:"scopes"`
+	jwt.StandardClaims
+}
+
+func getJwt(key Key, iss string, sub string, aud string, scopes []string, expiresIn int64) (string, error) {
+	k, err := key.GetDecoded()
+	if err != nil {
+		return "", &ApiError{
+			Params: map[string]interface{}{
+				"desc": "Error decoding key",
+			},
+			Err: err,
+		}
+	}
+
+	issuedAt := time.Now().Unix()
+
+	token := jwt.NewWithClaims(key.GetJwtSigningMethod(), claims{
+		Scopes: scopes,
+		StandardClaims: jwt.StandardClaims{
+			Issuer:    iss,
+			Subject:   sub,
+			Audience:  aud,
+			IssuedAt:  issuedAt,
+			ExpiresAt: issuedAt + expiresIn,
+		},
+	})
+	t, err := token.SignedString(k)
+	if err != nil {
+		return "", &ApiError{
+			Params: map[string]interface{}{
+				"desc": "Error signing JWT",
+			},
+			Err: err,
+		}
+	}
+	return t, nil
+}
