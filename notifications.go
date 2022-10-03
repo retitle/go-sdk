b124073083b5028b55eb9ea1674f38f2e2d488d4
ed-escudero <eduardo.escudero@compass.com>
2022-10-03 15:22:40 -0500

    Vers v3.2.0

---
+++ notifications.go
@@ -0,0 +1,29 @@
+package glide
+
+import (
+	"fmt"
+
+	"github.com/retitle/go-sdk/v3/core"
+)
+
+type NotificationsResource interface {
+	SendEmail(notification Notification, opts ...core.RequestOption) (*NotificationResponse, error)
+}
+
+type notificationsResourceImpl struct {
+	client Client
+}
+
+func GetNotificationsResource(client Client) NotificationsResource {
+	return notificationsResourceImpl{
+		client: client,
+	}
+}
+
+func (r notificationsResourceImpl) SendEmail(notification Notification, opts ...core.RequestOption) (*NotificationResponse, error) {
+	res := NotificationResponse{}
+	if err := r.client.Post(&res, true, fmt.Sprintf("/notifications/send_email"), notification, opts...); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
