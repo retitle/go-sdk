c7c5744183e7a035388a850a0c435fdab5241d5a
Julio Alves <julio.alves@compass.com>
2024-10-01 21:28:59 -0300

    Adds a new ClientVisibilityChangedAt field to Transaction Documents

--- v6/schemas.go
+++ v6/schemas.go
@@ -981,20 +981,21 @@ type TransactionCreator struct {
 }
 
 type TransactionDocument struct {
-	Id                     string       `json:"id,omitempty"`
-	ClientDocumentProperty string       `json:"client_document_property,omitempty"`
-	ClientDocumentType     string       `json:"client_document_type,omitempty"`
-	ClientVisibilityStatus string       `json:"client_visibility_status,omitempty"`
-	Folder                 *Folder      `json:"folder,omitempty"`
-	FolderKind             string       `json:"folder_kind,omitempty"`
-	LastModified           int          `json:"last_modified,omitempty"`
-	LatestVersionId        string       `json:"latest_version_id,omitempty"`
-	Order                  int          `json:"order,omitempty"`
-	PageCount              int          `json:"page_count,omitempty"`
-	Title                  string       `json:"title,omitempty"`
-	Transaction            *Transaction `json:"transaction,omitempty"`
-	Url                    string       `json:"url,omitempty"`
-	Object                 string       `json:"object,omitempty"`
+	Id                        string       `json:"id,omitempty"`
+	ClientDocumentProperty    string       `json:"client_document_property,omitempty"`
+	ClientDocumentType        string       `json:"client_document_type,omitempty"`
+	ClientVisibilityChangedAt int          `json:"client_visibility_changed_at,omitempty"`
+	ClientVisibilityStatus    string       `json:"client_visibility_status,omitempty"`
+	Folder                    *Folder      `json:"folder,omitempty"`
+	FolderKind                string       `json:"folder_kind,omitempty"`
+	LastModified              int          `json:"last_modified,omitempty"`
+	LatestVersionId           string       `json:"latest_version_id,omitempty"`
+	Order                     int          `json:"order,omitempty"`
+	PageCount                 int          `json:"page_count,omitempty"`
+	Title                     string       `json:"title,omitempty"`
+	Transaction               *Transaction `json:"transaction,omitempty"`
+	Url                       string       `json:"url,omitempty"`
+	Object                    string       `json:"object,omitempty"`
 }
 
 func (m TransactionDocument) IsRef() bool {
