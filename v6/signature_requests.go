8dad6946a210325b6fdc796c8555452159ffd5e1
hao-compass <hao.hoang@compass.com>
2025-04-09 10:44:24 +0700

    [TJ-46115] Add SetEsignTabConfig API to go-sdk  (#26)

--- v6/signature_requests.go
+++ v6/signature_requests.go
@@ -14,6 +14,7 @@ type SignatureRequestsResource interface {
 	Flow(signatureRequestFlowRequest SignatureRequestFlowRequest, opts ...core.RequestOption) (*SignatureRequestFlowResponse, error)
 	FlowDocuments(signatureRequestFlowDocumentsRequest SignatureRequestFlowDocumentsRequest, opts ...core.RequestOption) (*SignatureRequestFlowDocumentsResponse, error)
 	FlowDocumentsDetail(opts ...core.RequestOption) (*GetSignatureRequestFlowDocumentsResponse, error)
+	SaveTabConfig(signatureRequestSaveTabConfigRequest SignatureRequestSaveTabConfigRequest, opts ...core.RequestOption) (*SignatureRequestSaveTabConfigResponse, error)
 }
 
 type signatureRequestsResourceImpl struct {
@@ -75,6 +76,14 @@ func (r signatureRequestsResourceImpl) FlowDocuments(signatureRequestFlowDocumen
 func (r signatureRequestsResourceImpl) FlowDocumentsDetail(opts ...core.RequestOption) (*GetSignatureRequestFlowDocumentsResponse, error) {
 	res := GetSignatureRequestFlowDocumentsResponse{}
 	if err := r.client.Get(&res, true, fmt.Sprintf("/signature_requests/flow_documents_detail"), opts...); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+func (r signatureRequestsResourceImpl) SaveTabConfig(signatureRequestSaveTabConfigRequest SignatureRequestSaveTabConfigRequest, opts ...core.RequestOption) (*SignatureRequestSaveTabConfigResponse, error) {
+	res := SignatureRequestSaveTabConfigResponse{}
+	if err := r.client.Post(&res, true, fmt.Sprintf("/signature_requests/save_tab_config"), signatureRequestSaveTabConfigRequest, opts...); err != nil {
 		return nil, err
 	}
 	return &res, nil
