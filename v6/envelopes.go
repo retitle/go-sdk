f040cc0a24685864028bf11a17c2e62aa08d62b5
Hector Trejo <hector.trejo@compass.com>
2025-09-17 13:04:59 -0600

    refreshing go schemas for glide api (#67)

--- v6/envelopes.go
+++ v6/envelopes.go
@@ -7,21 +7,42 @@ import (
 )
 
 type EnvelopesResource interface {
+	EnvelopeDocument() EnvelopeDocumentResource
+	EnvelopeRecipient() EnvelopeRecipientResource
+	Step() StepResource
 	GetDetail(id string, opts ...core.RequestOption) (*Envelope, error)
 	Create(envelopeCreateIntentSchema EnvelopeCreateIntentSchema, opts ...core.RequestOption) (*EnvelopeCreateResponse, error)
-	StartRevision(id string, opts ...core.RequestOption) (*EnvelopeStartRevisionResponse, error)
-	CancelRevision(id string, opts ...core.RequestOption) (*EnvelopeCancelRevisionResponse, error)
+	CancelRevision(id string, envelopeCancelRevisionSchema EnvelopeCancelRevisionSchema, opts ...core.RequestOption) (*EnvCancelReviseResponse, error)
+	Revise(id string, envelopeStartRevisionSchema EnvelopeStartRevisionSchema, opts ...core.RequestOption) (*EnvStartReviseResponse, error)
 	Void(id string, envelopeVoidSchema EnvelopeVoidSchema, opts ...core.RequestOption) (*EnvelopeVoidResponse, error)
 }
 
 type envelopesResourceImpl struct {
-	client Client
+	client            Client
+	envelopeDocument  EnvelopeDocumentResource
+	envelopeRecipient EnvelopeRecipientResource
+	step              StepResource
 }
 
 func GetEnvelopesResource(client Client) EnvelopesResource {
 	return envelopesResourceImpl{
-		client: client,
+		client:            client,
+		envelopeDocument:  GetEnvelopeDocumentResource(client),
+		envelopeRecipient: GetEnvelopeRecipientResource(client),
+		step:              GetStepResource(client),
 	}
+}
+
+func (r envelopesResourceImpl) EnvelopeDocument() EnvelopeDocumentResource {
+	return r.envelopeDocument
+}
+
+func (r envelopesResourceImpl) EnvelopeRecipient() EnvelopeRecipientResource {
+	return r.envelopeRecipient
+}
+
+func (r envelopesResourceImpl) Step() StepResource {
+	return r.step
 }
 
 func (r envelopesResourceImpl) GetDetail(id string, opts ...core.RequestOption) (*Envelope, error) {
@@ -40,17 +61,17 @@ func (r envelopesResourceImpl) Create(envelopeCreateIntentSchema EnvelopeCreateI
 	return &res, nil
 }
 
-func (r envelopesResourceImpl) StartRevision(id string, opts ...core.RequestOption) (*EnvelopeStartRevisionResponse, error) {
-	res := EnvelopeStartRevisionResponse{}
-	if err := r.client.Post(&res, true, fmt.Sprintf("/envelopes/%s/revise", id), nil, opts...); err != nil {
+func (r envelopesResourceImpl) CancelRevision(id string, envelopeCancelRevisionSchema EnvelopeCancelRevisionSchema, opts ...core.RequestOption) (*EnvCancelReviseResponse, error) {
+	res := EnvCancelReviseResponse{}
+	if err := r.client.Post(&res, true, fmt.Sprintf("/envelopes/%s/cancelRevision", id), envelopeCancelRevisionSchema, opts...); err != nil {
 		return nil, err
 	}
 	return &res, nil
 }
 
-func (r envelopesResourceImpl) CancelRevision(id string, opts ...core.RequestOption) (*EnvelopeCancelRevisionResponse, error) {
-	res := EnvelopeCancelRevisionResponse{}
-	if err := r.client.Post(&res, true, fmt.Sprintf("/envelopes/%s/cancelRevision", id), nil, opts...); err != nil {
+func (r envelopesResourceImpl) Revise(id string, envelopeStartRevisionSchema EnvelopeStartRevisionSchema, opts ...core.RequestOption) (*EnvStartReviseResponse, error) {
+	res := EnvStartReviseResponse{}
+	if err := r.client.Post(&res, true, fmt.Sprintf("/envelopes/%s/revise", id), envelopeStartRevisionSchema, opts...); err != nil {
 		return nil, err
 	}
 	return &res, nil
