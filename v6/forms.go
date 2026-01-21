2605871e2821605aaa42f2f1e420a5d888a46051
Matias Fernandez <matias.fernandez@compass.com>
2026-01-21 14:23:00 -0500

    adding preview form method

--- v6/forms.go
+++ v6/forms.go
@@ -8,6 +8,7 @@ import (
 
 type FormsResource interface {
 	GetDetail(id string, opts ...core.RequestOption) (*TransactionFormsResponse, error)
+	PreviewForm(formId string, id string, opts ...core.RequestOption) (*BinaryResponse, error)
 }
 
 type formsResourceImpl struct {
@@ -23,6 +24,14 @@ func GetFormsResource(client Client) FormsResource {
 func (r formsResourceImpl) GetDetail(id string, opts ...core.RequestOption) (*TransactionFormsResponse, error) {
 	res := TransactionFormsResponse{}
 	if err := r.client.Get(&res, true, fmt.Sprintf("/transactions/%s/forms", id), opts...); err != nil {
+		return nil, err
+	}
+	return &res, nil
+}
+
+func (r formsResourceImpl) PreviewForm(formId string, id string, opts ...core.RequestOption) (*BinaryResponse, error) {
+	res := BinaryResponse{}
+	if err := r.client.GetStream(&res, true, fmt.Sprintf("/transactions/%s/forms/%s/preview_form", formId, id), opts...); err != nil {
 		return nil, err
 	}
 	return &res, nil
